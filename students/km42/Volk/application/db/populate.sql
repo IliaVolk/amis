04824bc11b91fa254e03f460fde3dbefdda05c83
Ilya Volk <ivolk@cloudlinux.com>
2017-12-27 23:11:11 +0200

    added handling uniquqe meeting constraint error, delete meeting action, serialization exception handling, user's email displaying

---
+++ students/km42/Volk/application/db/populate.sql
@@ -0,0 +1,13 @@
+INSERT INTO "User" ("name", "email") VALUES ('name1', 'liailia119@gmail.com');
+INSERT INTO "User" ("name", "email") VALUES ('name2', 'liailia1192@gmail.com');
+INSERT INTO "User" ("name", "email") VALUES ('name3', 'ivolk@cloudlinux.com');
+
+INSERT INTO "Meeting" ("title", "address", "email") VALUES ('title1', 'address1', 'liailia119@gmail.com');
+INSERT INTO "Meeting" ("title", "address", "email") VALUES ('title2', 'address2', 'liailia119@gmail.com');
+INSERT INTO "Meeting" ("title", "address", "email") VALUES ('title3', 'address3', 'ivolk@cloudlinux.com');
+
+INSERT INTO "Invitation" ("id", "email", "status") VALUES (1, 'liailia@gmail.com', 'pending');
+INSERT INTO "Invitation" ("id", "email", "status") VALUES (2, 'email3@gmail.com', 'pending');
+INSERT INTO "Invitation" ("id", "email", "status") VALUES (3, 'email3@gmail.com', 'accept');
+
+
